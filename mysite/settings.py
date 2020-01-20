898aafe9822090f5a84216f293e05ab9105537e2
José Gui <guibsonmouta15@gmail.com>
2020-01-20 18:27:50 -0300

    alterando o sttings

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'l4k)pn@e5dqseur)nakzd(%_m)d4406^@ae_)1iwvz951ri2jd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'jguif17.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'jguif18.pythonanywhere.com']
 
 
 # Application definition
